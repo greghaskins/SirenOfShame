23fa96d3ef1620c668f84b5d7535667c76f3c46d
Gregory Haskins <greg@greghaskins.com>
2015-10-23 18:30:00 -0400

    Create plugin for VisualStudioOnline REST API

---
+++ VisualStudioOnlineServices/VisualStudioOnlineEntryPoint.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.Composition;
+using SirenOfShame.Lib;
+using SirenOfShame.Lib.ServerConfiguration;
+using SirenOfShame.Lib.Settings;
+using SirenOfShame.Lib.Watcher;
+
+namespace VisualStudioOnlineServices
+{
+    [Export(typeof(ICiEntryPoint))]
+    public class VisualStudioOnlineEntryPoint : ICiEntryPoint
+    {
+        public ConfigureServerBase CreateConfigurationWindow(SirenOfShameSettings settings, CiEntryPointSetting ciEntryPointSetting)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string Name => "VisualStudioOnline";
+        public string DisplayName => "Visual Studio Online";
+        public WatcherBase GetWatcher(SirenOfShameSettings settings)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
